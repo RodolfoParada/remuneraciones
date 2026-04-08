7f99f7333ff57fad21c163893959b72ff21d49b6
Rodolfo Parada <rodolfo.parada.gonzalez>
2026-04-07 20:42:24 -0400

    feat: agrega Procfile

--- config/env.php
+++ config/env.php
@@ -1,12 +1,10 @@
 <?php
-
 // const DB_HOST    = '127.0.0.1';
 // const DB_PORT    = 3306;              
 // const DB_NAME    = 'remuneraciones';
 // const DB_USER    = 'root';
 // const DB_PASS    = 'admin';
 // const DB_CHARSET = 'utf8mb4';
-m
 define('DB_HOST',    getenv('MYSQLHOST')     ?: '127.0.0.1');
 define('DB_PORT',    getenv('MYSQLPORT')     ?: 3306);
 define('DB_NAME',    getenv('MYSQLDATABASE') ?: 'remuneraciones');
